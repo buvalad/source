f239345840f5c96ba21324b7c0644f9807544a6e
buvalad <165794309+buvalad@users.noreply.github.com>
2025-08-02 14:40:57 +0300

    Add files via upload

---
+++ Client/EterLocale/StringCodec_Vietnamese.h
@@ -0,0 +1,5 @@
+#pragma once
+
+int EL_String_Decode_Vietnamese(const char* multi, int multiLen, wchar_t* wide, int wideLen);
+int EL_String_Encode_Vietnamese(const wchar_t* wide, int wideLen, char* multi, int multiLen);
+//martysama0134's dcf42890919f0da1c0e6dbb7f15bc7ec
