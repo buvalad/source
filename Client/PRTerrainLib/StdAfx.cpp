3abbc6c4a0e7d3b9808c416aa05c27db74e82523
buvalad <165794309+buvalad@users.noreply.github.com>
2025-08-02 14:44:48 +0300

    Add files via upload

---
+++ Client/PRTerrainLib/StdAfx.cpp
@@ -0,0 +1,10 @@
+// stdafx.cpp : source file that includes just the standard includes
+//	PRTerrainLib.pch will be the pre-compiled header
+//	stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+/* Fast Float<->Integer conversion */
+float	PR_FCNV;
+long	PR_ICNV;
+//martysama0134's dcf42890919f0da1c0e6dbb7f15bc7ec
