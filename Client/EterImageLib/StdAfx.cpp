8845f734ead637c84acd376c7ea2b4c8ab573140
buvalad <165794309+buvalad@users.noreply.github.com>
2025-08-02 14:37:32 +0300

    Add files via upload

---
+++ Client/EterImageLib/StdAfx.cpp
@@ -0,0 +1,7 @@
+// stdafx.cpp : source file that includes just the standard includes
+//	eterImageLib.pch will be the pre-compiled header
+//	stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+namespace { char dummy; }; // solve warning lnk4221
+//martysama0134's dcf42890919f0da1c0e6dbb7f15bc7ec
