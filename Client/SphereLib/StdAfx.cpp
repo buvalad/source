e401e372aa75edd9f608da1134b0dc7c2fd7095a
buvalad <165794309+buvalad@users.noreply.github.com>
2025-08-02 14:45:58 +0300

    Add files via upload

---
+++ Client/SphereLib/StdAfx.cpp
@@ -0,0 +1,6 @@
+// stdafx.cpp : source file that includes just the standard includes
+//	SphereLib.pch will be the pre-compiled header
+//	stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+//martysama0134's dcf42890919f0da1c0e6dbb7f15bc7ec
