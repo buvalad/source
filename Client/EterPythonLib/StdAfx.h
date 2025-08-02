045a16f73899e524c300dd8edf558f95015b3129
buvalad <165794309+buvalad@users.noreply.github.com>
2025-08-02 14:41:50 +0300

    Add files via upload

---
+++ Client/EterPythonLib/StdAfx.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+#include "../UserInterface/Locale_inc.h"
+#include "../EterLib/StdAfx.h"
+#include "../ScriptLib/StdAfx.h"
+
+#include "PythonGraphic.h"
+#include "PythonWindowManager.h"
+
+void initgrp();
+void initgrpImage();
+void initgrpText();
+void initgrpThing();
+void initscriptWindow();
+void initwndMgr();
+//martysama0134's dcf42890919f0da1c0e6dbb7f15bc7ec
