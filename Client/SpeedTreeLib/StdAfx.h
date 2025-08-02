1c94c901ffa1cb9e1a590f9287d9d41877bda314
buvalad <165794309+buvalad@users.noreply.github.com>
2025-08-02 14:45:37 +0300

    Add files via upload

---
+++ Client/SpeedTreeLib/StdAfx.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
+
+#include <assert.h>
+#include "../UserInterface/Locale_inc.h"
+//#include <crtdbg.h>
+
+//#include "Forest.h"
+#include "SpeedTreeForestDirectX8.h"
+//martysama0134's dcf42890919f0da1c0e6dbb7f15bc7ec
